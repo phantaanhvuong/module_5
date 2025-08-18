7cdb78ef707599f6b25f3c2a579990b6803ca709
phantaanhvuong <khanhphan.201175@gmail.com>
2025-08-18 15:32:54 +0700

    nopbai

---
+++ bai_tap_ss2/display-student/src/App.jsx
@@ -0,0 +1,65 @@
+
+import './index.css'
+import React from "react";
+
+const Students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+]
+
+function App() {
+
+  return (
+    <>
+        <h1>Students</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+            </tr>
+            </thead>
+            <tbody>
+            {Students.map((student, index) => (
+                <tr key={index}>
+                    <td>{student.company}</td>
+                    <td>{student.contact}</td>
+                    <td>{student.country}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    </>
+  )
+}
+
+export default App
